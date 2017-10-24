178f8bf79492f1d00b7da75005b2db37344c1cdd
nattapong <58160388@go.buu.ac.th>
2017-10-24 23:11:05 +0700

    step 2

--- BookLookUP.spec.js
+++ BookLookUP.spec.js
@@ -1,4 +1,18 @@
+function BookLookUP(AmazonService) {
+    this.AmazonService = AmazonService
+
+    this.search = (isbn) => {
+        let SearchSuccess = this.AmazonService(isbn)
+        return SearchSuccess
+    }
+}
+
+
 test('BookLookUP', () => {
+
+    const AmazonService = jest.fn('123456789abcd')
+    .mockReturnValue({ name: 'Dota2', cover: '58160388@gmail.com', isbn:'123456789abcd'})
+
     
     let app = new BookLookUP(AmazonService)
     let isbn = '123456789abcd'
