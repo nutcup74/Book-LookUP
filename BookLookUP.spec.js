e75a24735412c942d0bc9994aba10577331c7d14
nattapong <58160388@go.buu.ac.th>
2017-10-24 23:08:51 +0700

    step 1

--- BookLookUP.spec.js
+++ BookLookUP.spec.js
@@ -1,4 +1,6 @@
-test('', () => {
-   
-  
-  })
+test('BookLookUP', () => {
+    
+    let app = new BookLookUP(AmazonService)
+    let isbn = '123456789abcd'
+    let value = app.search(isbn)  
+})
